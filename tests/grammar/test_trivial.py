ad3a3a6af8b5e254c6e7405940f098bd90e42bdb
Sergei Korolev <knopki@duck.com>
2023-10-06 22:04:53 +0300

    test: more tests

--- tests/grammar/test_trivial.py
+++ tests/grammar/test_trivial.py
@@ -6,22 +6,45 @@ import pytest
 
 from pytopas import TOPASParser, TOPASParseTree
 
+test_trivial_params = [
+    #
+    # NAME / PARAMETER_NAME
+    #
+    (
+        "some_name",
+        ("formula", [("NAME", "some_name")]),
+        "some_name",
+        False,
+    ),
+    ("some_name", "some_name", "some_name", True),
+    #
+    # parameter_value / PARAMETER_VAL
+    #
+    ("1", "1", "1", True),
+    ("1", ("formula", [("parameter_value", [("PARAMETER_VAL", "1")])]), "1", False),
+    #
+    # parameter_value / PARAMETER_VAL / OPERATOR (with whitespace test)
+    #
+    (
+        "1 + 1",
+        (
+            "formula",
+            [
+                ("parameter_value", [("PARAMETER_VAL", "1")]),
+                ("OPERATOR", "+"),
+                ("parameter_value", [("PARAMETER_VAL", "1")]),
+            ],
+        ),
+        "1 + 1",
+        False,
+    ),
+    ("1 + 1", "1 + 1", "1 + 1", True),
+]
+
 
 @pytest.mark.parametrize(
     "topas_in, obj_out, topas_out, compact",
-    [
-        # NAME / PARAMETER_NAME
-        (
-            "some_name",
-            ("formula", [("NAME", "some_name")]),
-            "some_name",
-            False,
-        ),
-        ("some_name", "some_name", "some_name", True),
-        # other
-        ("1", ("formula", [("parameter_value", [("PARAMETER_VAL", "1")])]), "1", False),
-        ("1", "1", "1", True),
-    ],
+    test_trivial_params,
 )
 def test_trivial(topas_in: str, obj_out: Any, topas_out: str, compact: bool):
     "Trivial grammar tests"
